45ce66de2d8c043100eb59044394201a973265bf
Nisha Lad <nishalad95@users.noreply.github.com>
2020-02-03 21:34:28 +0000

    adding script to scrape twitter in real time for specific hashtags, requires user to have local twitter config

---
+++ ScrapingTwitterRealTime/scrape.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## declare an array variable
+declare -a array=("brexit" "brexiteve" "leave" "remain" "eu" "referendum" "ukref" "voteleave" "voteremain" "boris" "farage")
+
+# get length of an array
+arraylength=${#array[@]}
+
+# use for loop to read all values and indexes
+for (( i=0; i<${arraylength}; i++ ));
+do
+  echo "---------------------------------------------"
+  echo "scraping twitter for: #${array[$i]}"
+  echo "---------------------------------------------"
+  echo ""
+  echo "python scrape.py -t" ${array[$i]} "-f scrape_data/${array[$i]}.csv -c config.yaml" 
+  python scrape.py -t ${array[$i]} -f scrape_data/${array[$i]}.csv -c config.yaml
+  echo "python scrape.py -t" ${array[$i]} "-f 31_Jan_2020_scrape_data/${array[$i]}.csv -c config.yaml" 
+  python 31_Jan_2020_scrape.py -t ${array[$i]} -f 31_Jan_2020_scrape_data/${array[$i]}.csv -c config.yaml
+  echo "---------------------------------------------"
+  echo "scrape ${i+1} / ${arraylength} complete"
+  echo "---------------------------------------------"
+  echo ""
+done
+
+echo "scrape complete!"
