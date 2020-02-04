84563efb247ebd439eec9125a34b6be1221214d5
Nisha Lad <nishalad95@users.noreply.github.com>
2020-02-04 16:36:09 +0000

    added instructions on how to run scrape.sh, cleaned up what the scraper pull and writes to csv, added time frame

--- ScrapingTwitterRealTime/scrape.py
+++ ScrapingTwitterRealTime/scrape.py
@@ -1,9 +1,13 @@
+# nlad 04/02/2020
+
 import tweepy
+import os
+import json
 import csv
-import pandas as pd
 import yaml
 import datetime
 from argparse import ArgumentParser
+
 
 parser = ArgumentParser(
         description="Scrape twitter API for a hashtag")
@@ -16,38 +20,72 @@ parser.add_argument(
         '-f',
         help='output csv file to save twitter data')
 parser.add_argument(
+        '--since',
+        '-s',
+        help='beginning of time period to scrape twitter data')
+parser.add_argument(
+        '--until',
+        '-u',
+        help='end of time period to scrape twitter data')
+parser.add_argument(
         '--config',
         '-c',
         help='config file containing twitter api authentication')
-args = parser.parse_args()
 
+
+# grab command line args
+args = parser.parse_args()
 hashtag = '#' + args.hashtag
 filename = args.filename
 config = args.config
+since = args.since
+until = args.until
+
 
 with open(config, 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
+
+# handle authentication
 consumer_key = cfg['consumerKey']
 consumer_secret = cfg['consumerSecret']
 access_token = cfg['accessToken']
 access_token_secret = cfg['accessTokenSecret']
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-# Open/Create a file to append data
-csvFile = open(filename, 'a')
+
+# open/create a file to append data
+if os.path.exists(filename):
+        append_write = 'a' # append if already exists
+else:
+        append_write = 'w' # make a new file if not
+csvFile = open(filename, append_write)
 csvWriter = csv.writer(csvFile)
+
 
 endTime = datetime.datetime.now() + datetime.timedelta(minutes=20)
 for i, tweet in enumerate(tweepy.Cursor(api.search,q=hashtag,count=100,
                            lang="en",
-                           since="2020-01-30",
-                           until="2020-01-31").items()):
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+                           since=since,
+                           until=until).items()):
+
+    tweets_encoded = tweet.text.encode('utf-8')
+    tweets_decoded = tweets_encoded.decode('utf-8')
+    csvWriter.writerow([tweet.created_at, tweet.id, tweets_decoded, tweet.entities, tweet.source,
+                        tweet._json["user"]["id"], tweet._json["user"]["screen_name"], 
+                        tweet._json["user"]["location"], tweet._json["user"]["followers_count"], 
+                        tweet._json["user"]["friends_count"],
+                        tweet._json["user"]["verified"], tweet._json["user"]["statuses_count"],
+                        tweet.geo, tweet.coordinates, tweet.place.name if tweet.place else None,
+                        tweet._json["user"]["location"], tweet.retweet_count, tweet.favorite_count, 
+                        tweet.favorited, tweet.retweeted
+                        ])
+
     if i % 1000 == 0:
         print("Tweet " + str(i) + " created at: " + str(tweet.created_at) + " saved")
     if datetime.datetime.now() >= endTime:
        break
+
