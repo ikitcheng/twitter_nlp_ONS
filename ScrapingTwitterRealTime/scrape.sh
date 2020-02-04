a133afcd33c08473bd1961fb83e3762cfd86165b
Nisha Lad <nishalad95@users.noreply.github.com>
2020-02-04 16:36:09 +0000

    added instructions on how to run scrape.sh, cleaned up what the scraper pull and writes to csv, added time frame

--- ScrapingTwitterRealTime/scrape.sh
+++ ScrapingTwitterRealTime/scrape.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-## declare an array variable
-declare -a array=("brexit" "brexiteve" "leave" "remain" "eu" "referendum" "ukref" "voteleave" "voteremain" "boris" "farage")
+# nlad 04/02/2020
 
-# get length of an array
-arraylength=${#array[@]}
+## declare hashtags variable
+declare -a hashtags=("brexit" "brexitshambles" "brexiteve" "leave" "remain" "eu" "referendum" 
+                  "ukref" "voteleave" "voteremain" "boris" "farage" "brexit50p" "brexiteers" "tory"
+                  "labour" "gotbrexitdone" "getbrexitdone" "bluecollarbritain" "bluewall" "notmybrexit")
 
-# use for loop to read all values and indexes
-for (( i=0; i<${arraylength}; i++ ));
+## time period to scrape
+declare -a since=("2020-01-28" "2020-01-29" "2020-01-30" "2020-01-31" "2020-02-01" "2020-02-02" "2020-02-03")
+
+declare -a until=("2020-01-29" "2020-01-30" "2020-01-31" "2020-02-01" "2020-02-02" "2020-02-03" "2020-02-04")
+
+# get length of arrays
+hashtaglength=${#hashtags[@]}
+sincelength=${#since[@]}
+
+
+for (( i=0; i<${sincelength}-5; i++ ));
 do
-  echo "---------------------------------------------"
-  echo "scraping twitter for: #${array[$i]}"
-  echo "---------------------------------------------"
-  echo ""
-  echo "python scrape.py -t" ${array[$i]} "-f scrape_data/${array[$i]}.csv -c config.yaml" 
-  python scrape.py -t ${array[$i]} -f scrape_data/${array[$i]}.csv -c config.yaml
-  #echo "python scrape.py -t" ${array[$i]} "-f 31_Jan_2020_scrape_data/${array[$i]}.csv -c config.yaml" 
-  #python 31_Jan_2020_scrape.py -t ${array[$i]} -f 31_Jan_2020_scrape_data/${array[$i]}.csv -c config.yaml
-  echo "---------------------------------------------"
-  echo "scrape ${i+1} / ${arraylength} complete"
-  echo "---------------------------------------------"
-  echo ""
+
+  dir="${since[$i]}-${until[$i]}"
+  mkdir -p scrape_data_${dir}
+
+  # use for loop to read all values and indexes
+  for (( j=0; j<${hashtaglength}-17; j++ ));
+  do
+
+    echo '---------------------------------------------'
+    echo "scraping twitter for: #${hashtags[$j]}"
+    echo $'--------------------------------------------- \n'
+    python scrape.py -t ${hashtags[$j]} -s ${since[$i]} -u ${until[$i]} -f scrape_data_${dir}/${hashtags[$j]}.csv -c config.yaml
+    echo $'\n---------------------------------------------'
+    echo "scrape ${j} / ${hashtaglength} complete"
+    echo $'--------------------------------------------- \n'
+
+  done
+
 done
 
 echo "scrape complete!"
