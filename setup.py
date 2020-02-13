5764ca393073e1b1351d71d44e2d583dc2d710c7
Nisha Lad <nishalad95@users.noreply.github.com>
2020-02-13 13:34:15 +0000

    changes to setup and compressing the datasets further

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name="twitter_nlp_ONS",
     version="0.1.0",
     packages=find_packages(exclude=['*tests']),
-    install_requires=['numpy', 'matplotlib'],
-    # author='Nisha Lad',
-    # author_email='nisha.lad.13@ucl.ac.uk',
-    # license='Copyright (c) 2019 Nisha Lad',
-    # description='Python travelplanner package',
-    # long_description=open('README.md').read(),
+    install_requires=['numpy', 'matplotlib', 'pandas', 'nltk'],
+    author='Nisha Lad, Matthew Cheng, Johannes Heyl',
+    author_email='nisha.lad.13@ucl.ac.uk, i.cheng.19@ucl.ac.uk, johannes.heyl.19@ucl.ac.uk',
+    license='Copyright (c) 2020 N. Lad, M. Cheng, J. Heyl',
+    description='NLP Analysis on Topical Twitter Data',
+    long_description=open('README.md').read(),
     # entry_points={
     #     'console_scripts': [
     #         'bussimula = travelplanner.command:process'
