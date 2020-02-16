60460ac454ef671980222f494299c8320f9dbd5a
Nisha Lad <nishalad95@users.noreply.github.com>
2020-02-16 21:10:11 +0000

    first attempt at extracting lat and long from the 'location' in the tweet and plotting this on an interactive map. wip: optimization with speed

--- GeoTagger/GeoTagger.py
+++ GeoTagger/GeoTagger.py
@@ -1,10 +1,11 @@
 import tarfile
 import pandas as pd
 import csv
+from geopy.geocoders import Nominatim
 
 
 # move this function to the cleaner?
-def read_and_merge_single_day_scraped_data(dataset, headers):
+def read_and_merge_scraped_data(dataset, headers):
     """
     Reads data scraped from Twitter & merges all data into a single dataframe
 
@@ -29,18 +30,78 @@ def read_and_merge_single_day_scraped_data(dataset, headers):
             hashtags_df = pd.concat([hashtags_df, current_hashtag])
         
         hashtags_df.columns = headers
+        tar.close()
         return hashtags_df
 
 
+def get_geo_info(place_name):
+    """
+    Gets coordinates and address for a given place name using geopy
+    """
+    # Create geo_locator object instance
+    geo_locator = Nominatim(user_agent="myGeocoder")
+
+    # place_name = "Champ de Mars, Paris, France"
+    # Attempt to obtain geo data for given place name
+    try:
+        location = geo_locator.geocode(place_name)
+    except Exception:
+        return None
+
+    if not location:
+        return None
+
+    # return float(location.latitude), float(location.longitude)
+    return pd.Series([location.latitude, location.longitude])
+
+
+
 # prepare the dataset we wish to analyse
-root_dir_data = "../Scraper/datasets/Brexit/All_Headers/"
-dataset = root_dir_data + "scrape_data_2020-01-29-2020-01-30.tar.gz"
-with open(root_dir_data + "headers.csv", newline='') as f:
-    reader = csv.reader(f)
-    headers = [row[0] for row in reader]
+# root_dir_data = "../Scraper/datasets/Brexit/All_Headers/"
+# dataset = root_dir_data + "scrape_data_2020-01-29-2020-01-30.tar.gz"
+# with open(root_dir_data + "headers.csv", newline='') as f:
+#     reader = csv.reader(f)
+#     headers = [row[0] for row in reader]
 
-hashtags_df = read_and_merge_single_day_scraped_data(dataset, headers)
+# hashtags_df = read_and_merge_scraped_data(dataset, headers)
 
-print(hashtags_df.head())
-print(hashtags_df.shape)
-print(hashtags_df['user.location'])
+# print(hashtags_df.head())
+# print(hashtags_df.shape)
+
+
+# unique_hashtags = hashtags_df['user.location'].unique()
+# print("Number of unique hashtags: ", len(unique_hashtags))
+# print(unique_hashtags)
+
+# print(hashtags_df['user.location'])
+# unique_locations = hashtags_df['user.location'].unique()
+# print("Number of unique locations: ", len(unique_locations))
+# print(unique_locations)
+
+# location = get_geo_info(unique_locations[-1])
+# print(location)
+# print(location.longitude)
+# print(location.latitude)
+
+# print(hashtags_df['user.location'].head())
+# print(type(hashtags_df['user.location'][0]))
+
+
+# nominatim_geo_location = [get_geo_info(location) for location in hashtags_df['user.location'].head()]
+# print(nominatim_geo_location)
+
+
+# hashtags_df['nomanatim_geo_location'] = hashtags_df['user.location'].apply(lambda x: get_geo_info(x))
+# print(hashtags_df.head())
+
+d = {'user.location': ['Ireland', 'London', 'Manchester', 'Wellingborough']}
+df = pd.DataFrame(data=d)
+print(df)
+
+df[['geo_lat', 'geo_long']] = df['user.location'].apply(lambda x: get_geo_info(x))
+print(df)
+
+import plotly.express as px
+fig = px.scatter_geo(df, lat=df['geo_lat'], lon=df['geo_long'])
+fig.show()
+
