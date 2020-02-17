093adf5e1e3e053a633718979f29ec22fd6b7d18
Johannes Heyl <johannesheyl@hotmail.com>
2020-02-17 21:43:38 +0000

    Updated setup.py file to include textblob package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="twitter_nlp_ONS",
     version="0.1.0",
     packages=find_packages(exclude=['*tests']),
-    install_requires=['numpy', 'matplotlib', 'pandas', 'nltk'],
+    install_requires=['numpy', 'matplotlib', 'pandas', 'nltk', 'textblob'],
     author='Nisha Lad, Matthew Cheng, Johannes Heyl',
     author_email='nisha.lad.13@ucl.ac.uk, i.cheng.19@ucl.ac.uk, johannes.heyl.19@ucl.ac.uk',
     license='Copyright (c) 2020 N. Lad, M. Cheng, J. Heyl',
